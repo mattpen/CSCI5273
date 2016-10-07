ee6e741a828f59fe7a913a2101d0769800cf17e8
mattpen <mape5853@colorado.edu>
2016-10-07 16:22:38 -0600

    added multithreading

--- PA2/webServer.cpp
+++ PA2/webServer.cpp
@@ -24,6 +24,7 @@
 #include <unistd.h>    //write
 #include <pthread.h> //for threading , link with lpthread
 #include <fstream>
+#include <sstream>
 
 struct Request {
   std::string method;
@@ -38,12 +39,11 @@ struct Config {
   int port;
   std::string docroot;
   std::string index;
-  std::unordered_map filetypes; // key is a file extension and value is a mime-type, ex {".txt":"text/plain"}
+  std::unordered_map <std::string, std::string> filetypes; // key is a file extension and value is a mime-type, ex {".txt":"text/plain"}
   int keepalivetime;
 };
 
 Config config;
-
 bool isWhiteSpace( char c ) {
   return c == ' ' || c == '\t';
 }
@@ -95,156 +95,164 @@ bool isCRLF( char c ) {
 // Parses string into object and validates method and version
 Request parseRequest( char *requestString, int requestString_len );
 
-Config getConfig();
+// Reads ./ws.conf and parses out the parameters for configuration.
+// Initializes the global config object
+void getConfig();
 
 //the thread function
-// void *connection_handler(void *);
+void *connection_handler(void *);
  
 int main(int argc , char *argv[])
 {
-    config = getConfig();
-    return 0;
+    getConfig();
 
+    int socket_desc , client_sock , c , *new_sock;
+    struct sockaddr_in server , client;
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( config.port );
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    listen(socket_desc , 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    {
+        puts("Connection accepted");
+         
+        pthread_t sniffer_thread;
+        new_sock = (int *) malloc(1);
+        *new_sock = client_sock;
+         
+        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+        {       
+            perror("could not create thread");
+            return 1;
+        }
+         
+        //Now join the thread , so that we dont terminate before the thread
+        //pthread_join( sniffer_thread , NULL);
+        puts("Handler assigned");
+    }
+     
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+     
+    return 0;
+}
+ 
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc)
+{
+    //Get the socket descriptor
+    int sock = *(int*)socket_desc;
+    int read_size;
+    std::string message;
+    const int MSG_SIZE = 2000;
+    char client_message[MSG_SIZE];
+     
+    //Send some messages to the client
+    message = "HTTP/1.1 200 OK\n\n<html><body>Hello World</body></html>\n";
+
+    //Receive a message from client
+    read_size = recv(sock, client_message, MSG_SIZE, 0);
+
+    printf("%s\n", client_message);
+
+    //Send the message back to client
+    send(sock , message.c_str(), message.length(), 0);
+    close(sock);
+     
+    if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+         
+    //Free the socket pointer
+    free(socket_desc);
+     
+    return 0;
 }
 
 
-//     int socket_desc , client_sock , c , *new_sock;
-//     struct sockaddr_in server , client;
-     
-//     //Create socket
-//     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-//     if (socket_desc == -1)
-//     {
-//         printf("Could not create socket");
-//     }
-//     puts("Socket created");
-     
-//     //Prepare the sockaddr_in structure
-//     server.sin_family = AF_INET;
-//     server.sin_addr.s_addr = INADDR_ANY;
-//     server.sin_port = htons( 8888 );
-     
-//     //Bind
-//     if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-//     {
-//         //print the error message
-//         perror("bind failed. Error");
-//         return 1;
-//     }
-//     puts("bind done");
-     
-//     //Listen
-//     listen(socket_desc , 3);
-     
-//     //Accept and incoming connection
-//     puts("Waiting for incoming connections...");
-//     c = sizeof(struct sockaddr_in);
-     
-     
-//     //Accept and incoming connection
-//     puts("Waiting for incoming connections...");
-//     c = sizeof(struct sockaddr_in);
-//     while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-//     {
-//         puts("Connection accepted");
-         
-//         pthread_t sniffer_thread;
-//         new_sock = malloc(1);
-//         *new_sock = client_sock;
-         
-//         if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
-//         {
-//             perror("could not create thread");
-//             return 1;
-//         }
-         
-//         //Now join the thread , so that we dont terminate before the thread
-//         //pthread_join( sniffer_thread , NULL);
-//         puts("Handler assigned");
-//     }
-     
-//     if (client_sock < 0)
-//     {
-//         perror("accept failed");
-//         return 1;
-//     }
-     
-//     return 0;
-// }
- 
-// /*
-//  * This will handle connection for each client
-//  * */
-// void *connection_handler(void *socket_desc)
-// {
-//     //Get the socket descriptor
-//     int sock = *(int*)socket_desc;
-//     int read_size;
-//     char *message , client_message[2000];
-     
-//     //Send some messages to the client
-//     message = "Greetings! I am your connection handler\n";
-//     write(sock , message , strlen(message));
-     
-//     message = "Now type something and i shall repeat what you type \n";
-//     write(sock , message , strlen(message));
-     
-//     //Receive a message from client
-//     while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
-//     {
-//         //Send the message back to client
-//         write(sock , client_message , strlen(client_message));
-//     }
-     
-//     if(read_size == 0)
-//     {
-//         puts("Client disconnected");
-//         fflush(stdout);
-//     }
-//     else if(read_size == -1)
-//     {
-//         perror("recv failed");
-//     }
-         
-//     //Free the socket pointer
-//     free(socket_desc);
-     
-//     return 0;
-// }
 
 /**
   *
   **/
-Config getConfig() {
+void getConfig() {
   std::ifstream infile("ws.conf");
-  std::string line;
-  Config config;
+  std::string line; // Used to read a line of the configuration file
+  std::string key;
+  std::string val;
+  std::size_t first_whitespace = 0; // Used to parse extension from ContentType
+  std::size_t second_whitespace = 0; // Used to parse mime-type from ContentType
+
   while (std::getline(infile, line))
   {
       std::istringstream iss(line);
       try {
-        if ( line.find( "ListenPort" ) == 0 ) {
-          config.port == std::stoi( line.substr( line.rfind(" "), line.length() - line.rfind(" ") ) );
+        if ( line.find( "#" ) == 0 ) {
+          // Don't try to parse comments
+          continue;
+        }
+        else if ( line.find( "ListenPort" ) == 0 ) {
+          config.port = std::stoi( line.substr( line.rfind(" ") + 1, line.length() - line.rfind(" ") - 1 ) );
         }
         else if ( line.find( "KeepaliveTime" ) == 0 ) {
-          config.keepalivetime == std::stoi( line.substr( line.rfind(" "), line.length() - line.rfind(" ") ) );
+          config.keepalivetime = std::stoi( line.substr( line.rfind(" "), line.length() - line.rfind(" ") ) );
         }
         else if ( line.find( "DocumentRoot" ) == 0 ) {
-          config.docroot == line.substr( line.rfind(" "), line.length() - line.rfind(" ") );
+          config.docroot = line.substr( line.rfind(" "), line.length() - line.rfind(" ") );
         }
         else if ( line.find( "DirectoryIndex" ) == 0 ) {
-          config.index == line.substr( line.rfind(" "), line.length() - line.rfind(" ") );
+          config.index = line.substr( line.rfind(" "), line.length() - line.rfind(" ") );
         }
         else if ( line.find( "ContentType" ) == 0 ) {
-          //TODO: finish this
+          first_whitespace = line.find(" ");
+          second_whitespace = line.find(" ", first_whitespace + 1);
+          key = line.substr( first_whitespace + 1, second_whitespace - first_whitespace - 1);
+          val = line.substr( second_whitespace + 1, line.length() - second_whitespace - 1 );
+          config.filetypes[ key ] = val;
+        }
+        else {
+          // Add support for additional directives here
         }
       }
       catch (int e) {
-        printf("Error occurred reading line: %s in config, error message: %d\n", line, e);
+        // If configuration parse fails then exit immediately
+        printf("Error occurred reading line: %s in config, error message: %d\n", line.c_str(), e);
         exit(1);
       }
   }
-  return config;
+  
+  return;
 }
 
 /**
