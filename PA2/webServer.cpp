c6c72fe2c950376595706f8f5c37389874a54bd6
mattpen <mape5853@colorado.edu>
2016-10-07 22:52:41 -0600

    Not sure what happened here

--- PA2/webServer.cpp
+++ PA2/webServer.cpp
@@ -104,66 +104,66 @@ void *connection_handler(void *);
  
 int main(int argc , char *argv[])
 {
-    getConfig();
+  getConfig();
 
-    int socket_desc , client_sock , c , *new_sock;
-    struct sockaddr_in server , client;
+  int socket_desc , client_sock , c , *new_sock;
+  struct sockaddr_in server , client;
+   
+  //Create socket
+  socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+  if (socket_desc == -1)
+  {
+    printf("Could not create socket");
+  }
+  puts("Socket created");
+   
+  //Prepare the sockaddr_in structure
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+  server.sin_port = htons( config.port );
+
+  //Bind
+  if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+  {
+    //print the error message
+    perror("bind failed. Error");
+    return 1;
+  }
+  puts("bind done");
+   
+  //Listen
+  listen(socket_desc , 3);
+
+  //Accept and incoming connection
+  puts("Waiting for incoming connections...");
+  c = sizeof(struct sockaddr_in);
+
+  while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+  {
+    puts("Connection accepted");
+       
+    pthread_t sniffer_thread;
+    new_sock = (int *) malloc(1);
+    *new_sock = client_sock;
      
-    //Create socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-     
-    //Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( config.port );
-
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        //print the error message
-        perror("bind failed. Error");
-        return 1;
-    }
-    puts("bind done");
-     
-    //Listen
-    listen(socket_desc , 3);
-
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
-
-    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-    {
-        puts("Connection accepted");
-         
-        pthread_t sniffer_thread;
-        new_sock = (int *) malloc(1);
-        *new_sock = client_sock;
-         
-        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
-        {       
-            perror("could not create thread");
-            return 1;
-        }
-         
-        //Now join the thread , so that we dont terminate before the thread
-        //pthread_join( sniffer_thread , NULL);
-        puts("Handler assigned");
+    if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+    {       
+      perror("could not create thread");
+      return 1;
     }
      
-    if (client_sock < 0)
-    {
-        perror("accept failed");
-        return 1;
-    }
-     
-    return 0;
+    //Now join the thread , so that we dont terminate before the thread
+    //pthread_join( sniffer_thread , NULL);
+    puts("Handler assigned");
+  }
+   
+  if (client_sock < 0)
+  {
+    perror("accept failed");
+    return 1;
+  }
+   
+  return 0;
 }
  
 /*
@@ -171,34 +171,35 @@ int main(int argc , char *argv[])
  * */
 void *connection_handler(void *socket_desc)
 {
-    //Get the socket descriptor
-    int sock = *(int*)socket_desc;
-    int read_size;
-    std::string message;
-    const int MSG_SIZE = 2000;
-    char client_message[MSG_SIZE];
-     
-    //Send some messages to the client
-    message = "HTTP/1.1 200 OK\n\n<html><body>Hello World</body></html>\n";
+  //Get the socket descriptor
+  int sock = *(int*)socket_desc;
+  int read_size;
+  std::string message;
+  const int MSG_SIZE = 2000;
+  char client_message[MSG_SIZE];
+  Request request;
 
-    //Receive a message from client
-    read_size = recv(sock, client_message, MSG_SIZE, 0);
+  //Receive a message from client
+  read_size = recv(sock, client_message, MSG_SIZE, 0);
 
-    printf("%s\n", client_message);
+  request = parseRequest( client_message, strlen(client_message) );
 
-    //Send the message back to client
-    send(sock , message.c_str(), message.length(), 0);
-    close(sock);
-     
-    if(read_size == -1)
-    {
-        perror("recv failed");
-    }
-         
-    //Free the socket pointer
-    free(socket_desc);
-     
-    return 0;
+  //Send some messages to the client
+  message = "HTTP/1.1 200 OK\n\n<html><body>Hello World<br/>";
+  //Send the message back to client
+  send(sock , message.c_str(), message.length(), 0);
+
+  close(sock);
+   
+  if(read_size == -1)
+  {
+    perror("recv failed");
+  }
+       
+  //Free the socket pointer
+  free(socket_desc);
+   
+  return 0;
 }
 
 
