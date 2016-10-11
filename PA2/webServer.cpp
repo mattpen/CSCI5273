c4d635093b3753959caed57eb7ceffe1a3943039
mattpen <mape5853@colorado.edu>
2016-10-10 19:35:02 -0600

    moar fixes

--- PA2/webServer.cpp
+++ PA2/webServer.cpp
@@ -69,10 +69,6 @@ void *connection_handler(void *);
 
 //socket close function
 void ctrlc_handler(int s);
-
-int getSO_ERROR(int fd);
-
-void closeSocket(int fd);
  
 int main(int argc , char *argv[])
 {
@@ -148,10 +144,10 @@ int main(int argc , char *argv[])
   printf("Closing threads...\n");
   while ( !socks.empty() ) {
     printf( "Threads remaining: %ld\n", socks.size() );
-    closeSocket( socks.back() );
+    close( socks.back() );
     socks.pop();    
   }
-  closeSocket( socket_desc );
+  close( socket_desc );
   return 0;
 }
  
@@ -469,25 +465,4 @@ Request parseRequest( char *requestString, int requestString_len ) {
   }
 
   return request;
-}
-
-int getSO_ERROR(int fd) {
-   int err = 1;
-   socklen_t len = sizeof err;
-   if (-1 == getsockopt(fd, SOL_SOCKET, SO_ERROR, (char *)&err, &len))
-      perror("getSO_ERROR");
-   if (err)
-      errno = err;              // set errno to the socket SO_ERROR
-   return err;
-}
-
-void closeSocket(int fd) {      // *not* the Windows closesocket()
-   if (fd >= 0) {
-      getSO_ERROR(fd); // first clear any errors, which can cause close to fail
-      if (shutdown(fd, SHUT_RDWR) < 0) // secondly, terminate the 'reliable' delivery
-         if (errno != ENOTCONN && errno != EINVAL) // SGI causes EINVAL
-            perror("shutdown");
-      if (close(fd) < 0) // finally call close()
-         perror("close");
-   }
 }
